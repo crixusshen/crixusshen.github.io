ec93a60d2d1fc9802b74cbd227e29c4ad8c5860f
dfzq <dfzq@MacBookPro.local>
2019-06-21 15:30:28 +0800

    no message

---
+++ precache-manifest.08d506683b6f276a151846f824277090.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "319ba70adca69e98caf82e7e8042a4f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba13f4dbcd926e32afea",
+    "url": "/static/css/main.f43453ae.chunk.css"
+  },
+  {
+    "revision": "f5a829b8316b35de0789",
+    "url": "/static/js/2.53cd048c.chunk.js"
+  },
+  {
+    "revision": "ba13f4dbcd926e32afea",
+    "url": "/static/js/main.b8c2d3d2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e5aa9b11dfb5ae932fd0ceba9d09aa81.js"
+  "/precache-manifest.08d506683b6f276a151846f824277090.js"
 );
 
 self.addEventListener('message', (event) => {
