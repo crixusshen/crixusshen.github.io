4a000d5bf2e9bee9fd555d71d1e0d4ad6c43df69
dfzq <dfzq@MacBookPro.local>
2019-06-21 15:05:49 +0800

    no message

---
+++ bug-vivo/build/precache-manifest.139e74063c3bcb863fa0cf8ff68ab28d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbdddd3341afb51e540a113b531b4139",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ef0b18385feb06eadf8",
+    "url": "/static/css/main.f43453ae.chunk.css"
+  },
+  {
+    "revision": "fc1a04f5442fc70ce71f",
+    "url": "/static/js/2.f9075ab4.chunk.js"
+  },
+  {
+    "revision": "2ef0b18385feb06eadf8",
+    "url": "/static/js/main.78c6d065.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
