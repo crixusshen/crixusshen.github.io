cdb077947fa6d896de31db956ac17a5254b6f18b
dfzq <dfzq@MacBookPro.local>
2019-06-24 12:23:58 +0800

    no message

---
+++ README.md
@@ -0,0 +1,11 @@
+# JS-SDK For OrientSec.UED
+
+东方赢家财富版APP桥接程序调用示例         
+
+开发框架 | 具体查看
+:- | :- 
+无 | ./window.html
+React | ./react-app/src/App.js
+Vue | ./vue-app/src/App.vue
+
+其它框架请参考./window.html实现。        
---
+++ react-app/precache-manifest.5807f8ef429ae38ec49e744e89fe710c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e56844e6e86e85c13021483f2ffa081",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a4f7351aaed49c21eba",
+    "url": "/static/css/main.078d7a2b.chunk.css"
+  },
+  {
+    "revision": "7a4c920d7bfe3b46cd6b",
+    "url": "/static/js/2.57621803.chunk.js"
+  },
+  {
+    "revision": "1a4f7351aaed49c21eba",
+    "url": "/static/js/main.0f397fd6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
